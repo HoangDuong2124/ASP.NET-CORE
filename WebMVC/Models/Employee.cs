cfc75a008186f677f8e3d26483cf8fe74430b011
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2022-12-17 23:18:58 +0700

    Exam
    Exam

---
+++ WebMVC/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMVC.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Required]
+        public string? EmployeeId { get; set; }
+        [Required]
+        public string? EmployeeName { get; set; }
+        [Required]
+        public string? Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ WebMVC/Models/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebMVC.Data;
+using System;
+using System.Linq;
+
+namespace WebMVC.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new WebMVCContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<WebMVCContext>>()))
+            {
+                // Look for any movies.
+                if (context.Employee.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Employee.AddRange(
+                    new Employee
+                    {
+                        EmployeeId = "MVC001",
+                         EmployeeName = "Trần Văn A",
+                        Department = "HR",
+                        Salary = 1000
+                    },
+                     new Employee
+                     {
+                         EmployeeId = "MVC002",
+                         EmployeeName = "Trần Nhật Tân",
+                         Department = "SC",
+                         Salary = 1050
+                     },
+                      new Employee
+                      {
+                          EmployeeId = "MVC003",
+                          EmployeeName = "Nguyễn Văn A",
+                          Department = "HR",
+                          Salary = 2000
+                      },
+                       new Employee
+                       {
+                           EmployeeId = "MVC004",
+                           EmployeeName = "Trần Bình Dương",
+                           Department = "MD",
+                           Salary = 1111
+                       }
+
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
